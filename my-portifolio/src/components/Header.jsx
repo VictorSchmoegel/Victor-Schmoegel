ed935abe650cd7aa5f122260c967d09ab55f524f
Victor Schmoegel <victorschmoegel@gmail.com>
2023-08-11 14:04:12 -0300

    feito menu mobile

--- my-portifolio/src/components/Header.jsx
+++ my-portifolio/src/components/Header.jsx
@@ -1,19 +1,76 @@
-import React from "react";
-import styles from "./Header.module.css";
+import React, { useState } from "react";
 import { Link } from "react-scroll";
+import './Header.css';
+import { FaBars } from 'react-icons/fa';
 
-function header() {
+function Header() {
+  const [navbarIsOpen, setNavbarIsOpen] = useState(false);
+
+  const mobileMenu = () => {
+    setNavbarIsOpen((prevNavbarIsOpen) => !prevNavbarIsOpen);
+  }
+
   return (
-    <nav className={styles.navbar}>
-      <h3>Victor Schmoegel.dev</h3>
-      <ul>
-        <li><Link to="home" spy={true} smooth={true} offset={50} duration={700}>Home</Link></li>
-        <li><Link to="about" spy={true} smooth={true} offset={50} duration={700}>About</Link></li>
-        <li><Link to="projects" spy={true} smooth={true} offset={50} duration={700}>Projects</Link></li>
-        <li><Link to="contact" spy={true} smooth={true} offset={50} duration={700}>Contact</Link></li>
-      </ul>
+    <nav className='navbar_central'>
+        <h3 className="nav-title">Victor Schmoegel.dev</h3>
+        <li
+          className="mobile-menu"
+          onClick={mobileMenu}
+        >
+          <FaBars />
+        </li>
+        <ul className={`links-list ${navbarIsOpen ? "open" : ""}`}>
+          <li className="nav-pages">
+            <Link
+              to="home"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={700}
+              onClick={() => setNavbarIsOpen(false)}
+            >
+              Home
+            </Link>
+          </li>
+          <li className="nav-pages">
+            <Link
+              to="about"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={700}
+              onClick={() => setNavbarIsOpen(false)}
+            >
+              About
+            </Link>
+          </li>
+          <li className="nav-pages">
+            <Link
+              to="projects"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={700}
+              onClick={() => setNavbarIsOpen(false)}
+            >
+              Projects
+            </Link>
+          </li>
+          <li className="nav-pages">
+            <Link
+              to="contact"
+              spy={true}
+              smooth={true}
+              offset={50}
+              duration={700}
+              onClick={() => setNavbarIsOpen(false)}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
     </nav>
   );
 }
 
-export default header;
+export default Header;
